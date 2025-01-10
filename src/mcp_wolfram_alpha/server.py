8f065f8e6b77ab92d6745a522ca3fa5a9ccd9873
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2025-01-10 20:09:59 +0000

    version bump

--- src/mcp_wolfram_alpha/server.py
+++ src/mcp_wolfram_alpha/server.py
@@ -134,7 +134,7 @@ async def main():
             write_stream,
             InitializationOptions(
                 server_name="MCP-wolfram-alpha",
-                server_version="0.1.0",
+                server_version="0.2.1",
                 capabilities=server.get_capabilities(
                     notification_options=NotificationOptions(),
                     experimental_capabilities={},
